4464e4b00ac1b76ffb0b77847c7bb9aef0e67253
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 22:35:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  unless 
+  #if block_given?
+    i = 0
+    arr = []
+    while i <= words.length
+      yield words.at(i)
+      i += 1
+      break words if i == words.length
+    end
+  #else
+  #  'This block should not run!'
+  #end
 end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) { |i|  i }
+
+my_each(['arel', 'jon', 'logan', 'spencer']) { |i| i }
